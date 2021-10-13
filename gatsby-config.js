8f60081cf37d35b4855f65a078fa933467933f2a
Matthew Lee <matthewlee@Matthews-MBP.localdomain>
2021-10-13 15:19:44 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="../images/mountain_bw.jpg"
+        alt="Black and White picture of snowy mountain in the distance"
+      />
     </Layout>
   )
 }
